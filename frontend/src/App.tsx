2213677405de4c41f897afee490db7fcf2842a6d
J.W <91964698+expertcomputeruser@users.noreply.github.com>
2023-08-31 12:08:50 -0500

    Created basic react app and set up proxy to Spring rest controller.

---
+++ frontend/src/App.tsx
@@ -0,0 +1,40 @@
+import React, {useState, useEffect} from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+
+interface demoResponseJson {
+    headerText: string,
+    bodyText: string,
+}
+function App() {
+
+    const [json, setJson] = useState<demoResponseJson>();
+
+    async function fetchContent() {
+        const response = await fetch("/api/")
+        console.log("fetchContent", response);
+        return await response.json();
+    }
+
+    useEffect(() => {
+        fetchContent().then((res) => {
+            console.log("useEffect", res)
+            setJson(res);
+        })
+    }, []);
+
+
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>{json?.headerText}</h1>
+          <p>{json?.bodyText}</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
